56e13ef79b202649b89d2bcbc0c9b5b6912c8b58
ArtiOnMoon <67146003+ArtiOnMoon@users.noreply.github.com>
2024-03-16 15:49:06 +0300

    123

---
+++ Task 1/Circle.cs
@@ -0,0 +1,32 @@
+﻿namespace Task_1
+{
+   
+         static class Circle
+    
+        {
+            static  public string GetLenth(double r)
+            {
+
+                return $"Длина окружности {2 * Math.PI * r}";
+
+
+
+            }
+        static public string GetSquare(double r)
+            {
+                
+                    return $"Площадь окружности {Math.PI * r*r}";
+
+
+            }
+
+        static public string IsPoinHere (double r,double x, double y, double x0, double y0)
+            {
+                if ((x - x0)* (x - x0) + (y - y0)* (y - y0) <= r*r)
+                return "Точка принадлежит окружности";
+
+                else
+                return "Точка не принадлежит окружности";
+            }
+        }
+    }
--- Task 1/Program.cs
+++ Task 1/Program.cs
@@ -1,10 +1,27 @@
 ﻿namespace Task_1;
+//Разработать статический класс для работы с окружностью. Класс должен содержать 3 метода:
+
+//метод, определяющий длину окружности по заданному радиусу;
+//метод, определяющий площадь круга по заданному радиусу;
+//метод, проверяющий принадлежность точки с координатами(x, y) кругу с радиусом r и координатами центра x0, y0.
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Введите радиус");
+        double r = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Введите координаты точки");
+        double x = Convert.ToDouble(Console.ReadLine());
+        double y = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Введите координаты центра круга");
+        double x0 = Convert.ToDouble(Console.ReadLine());
+        double y0 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine(Circle.GetLenth(r));
+        Console.WriteLine(Circle.GetSquare(r));
+        Console.WriteLine(Circle.IsPoinHere(r,x,y,x0,y0));
+        Console.ReadKey();
+
     }
 }
 
